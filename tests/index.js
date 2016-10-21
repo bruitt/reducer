92d1f51a5e66406fc93beb30a40aedae8711d746
Andrey Mayorov <frandsaw@gmail.com>
2016-10-21 13:00:21 +0300

    Add handlers prefix test

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,8 @@
 import test from 'tape'
 import createReducer, * as utils from '../src/index'
 
+let identity = (value) => value
+
 test('transform camel case to standard flux type const', (t) => {
   let { camelToConst } = utils
   let cases = {
@@ -21,5 +23,30 @@ test('transform camel case to standard flux type const', (t) => {
 test('prefix type', (t) => {
   let { prefixType } = utils
   t.equal('@test/TEST_ACTION', prefixType('@test', 'testAction'), 'testAction -> @test/TEST_ACTION')
+  t.end()
+})
+
+
+test('transform types to prefixed types', (t) => {
+  let { getPrefixedTypes } = utils
+  let handlers = {
+    addTodo: identity,
+    removeTodo: identity
+  }
+
+  let result = getPrefixedTypes('@todo', handlers)
+  let expectedResult = {
+    '@todo/ADD_TODO': {
+      originalType: 'addTodo',
+      prefixedType: '@todo/ADD_TODO'
+    },
+
+    '@todo/REMOVE_TODO': {
+      originalType: 'removeTodo',
+      prefixedType: '@todo/REMOVE_TODO'
+    }
+  }
+
+  t.deepEqual(expectedResult, result)
   t.end()
 })
